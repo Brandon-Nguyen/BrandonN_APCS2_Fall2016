7f09f004642521e9bd51735f8ac87bec19af4aa6
Brandon B. Nguyen <18nguyenb20@millshighsch.org>
2016-08-31 20:00:23 -0700

    up to foil for math library 8/31/16

--- Access Calculate/src/DoMath.java
+++ Access Calculate/src/DoMath.java
@@ -13,5 +13,10 @@ public class DoMath {
 		System.out.println(Calculate.average(2, 6));
 		System.out.println(Calculate.average(4, 12, 8));
 		System.out.println(Calculate.toDegrees(3));
+		System.out.println(Calculate.toRadians(180.0));
+		System.out.println(Calculate.discriminant(2, 3, 4));
+		System.out.println(Calculate.toImproperFrac(3, 1, 2));
+		System.out.println(Calculate.toMixedNum(7, 2));
+		System.out.println(Calculate.foil(2,  3,  6 , -7 , "n"));
 	}
 }
